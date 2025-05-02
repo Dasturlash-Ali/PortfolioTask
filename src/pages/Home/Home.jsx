50e6cc65609101153cf60a0c21f4f1f34650f250
Muzaffar <aliyevmuhammadzafar@gmail.com>
2025-05-02 16:26:24 +0500

    update code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './Home.scss';
+import VectorImg from '../../assets/Vector.svg'
+import BrandsAd from './BrandsAd';
 
 function Home() {
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className='hero'>
+      <div className='container'>
+        <h2>
+          <span>FIND CLOTHES</span>
+          <span>THAT MATCHES</span>
+          <span>YOUR STYLE</span>
+        </h2>
+
+        <div className='home-p-img'>
+          <p>Browse through our diverse range of meticulously crafted garments, designed <br /> to bring out your individuality and cater to your sense of style.</p>
+          <img src={VectorImg}alt="This home icon StarIcon" />
+        </div>
+
+        <img className='bigStarIcon' src={VectorImg} alt="This home icon StarIcon" />
+        
+        <button className='shop-btn'>Shop Now</button>
+
+        <table className='nomeric-wrapper'>
+          <div className='nomeric'>
+            <h3>200+</h3>
+            <p>International Brands</p>
+          </div>
+          <div className='nomeric'>
+            <h3>2,000+</h3>
+            <p>High-Quality Products</p>
+          </div>
+          <div className='nomeric'>
+            <h3>30,000+</h3>
+            <p>Happy Customers</p>
+          </div>
+        </table>
+      </div>
+      <div>
+        <BrandsAd />
+      </div>
     </div>
   )
 }
