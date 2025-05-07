1a774a3e312449bea9004b4d77dd1e7845e5d092
Muzaffar <aliyevmuhammadzafar@gmail.com>
2025-05-07 16:41:10 +0500

    update code

--- src/pages/Home/ByDress.jsx
+++ src/pages/Home/ByDress.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ByDress.scss';
 import { BiSolidStar } from 'react-icons/bi';
 import { GoArrowLeft, GoArrowRight } from 'react-icons/go';
+import { FcApproval } from 'react-icons/fc';
 
 function ByDress() {
   return (
@@ -19,6 +20,9 @@ function ByDress() {
           </div>
         </div>
       </div>
+
+      {/*------------------------------- OUR HAPPY CUSTOMERS--------------------------------------- */}
+      
       <div className='coments'>
         <div className='customers'>
             <h3>OUR HAPPY CUSTOMERS</h3>
@@ -27,15 +31,49 @@ function ByDress() {
                 <GoArrowRight />
             </div>
         </div>
-        <div className='coment-wrapper'>
-            <div className='stars'>
-                <BiSolidStar className='star-icon'/>
-                <BiSolidStar className='star-icon'/>
-                <BiSolidStar className='star-icon'/>
-                <BiSolidStar className='star-icon'/>
-                <BiSolidStar className='star-icon'/>
-                <p className='raiting'>Sarah M.</p>
-                <p></p>
+        <div className='our-happy'>
+            <div className='coment-wrapper'>
+                <div className='stars'>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <p>Sarah M.<FcApproval /></p>
+                    <span>
+                        "I'm blown away by the quality and style of the clothes I received from Shop.co. 
+                        From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”
+                    </span>
+                </div>
+            </div>
+            <div className='coment-wrapper'>
+                <div className='stars'>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <p className='raiting'>Alex K.<FcApproval /></p>
+                    <span>
+                        "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. 
+                        The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”
+                    </span>
+                </div>
+            </div>
+            <div className='coment-wrapper'>
+                <div className='stars'>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <BiSolidStar className='star-icon'/>
+                    <p className='raiting'>James L.<FcApproval /></p>
+                    <span>
+                        "As someone who's always on the lookout for unique fashion pieces, 
+                        I'm thrilled to have stumbled upon Shop.co. 
+                        The selection of clothes is not only diverse but also on-point with the latest trends.”
+                    </span>
+                </div>
             </div>
         </div>
       </div>
